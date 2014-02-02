dc27bfc15f52a5de37f0ad6de8340277ab5437a8
Sami Abdelgadir <simsimzone@yahoo.co.uk>
2014-02-02 23:45:36 +0300

    Update Source.c

--- Source.c
+++ Source.c
@@ -1,15 +1,23 @@
-
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
+char** str_split(char *, char *);
+int main()
+{
+	printf("\n");
+	char *delimeters = ":,";
+	char **result = str_split("sami:abdelgadir:mohammed", ":");
+	while (*result)
+	{
+		printf("%s\t", *result++);
+	}
+}
 
 char *str_dup(const char *);
 int str_any_char(char *, char);
 
-char** strsplit(char *str, char *delimeters)
+char** str_split(char *str, char *delimeters)
 {
 	char **result;
 	char *input = str_dup(str);		/* so as to work with a copy of str and let str be unmodified */
@@ -36,32 +44,11 @@ char** strsplit(char *str, char *delimeters)
 	return result;
 }
 
-int main()
-{
-	printf("\n");
-	char *delimeters = ":,";
-	char **result = strsplit("sami:abdelgadir:mohammed", ":");
-	while (*result)
-	{
-		printf("%s\t", *result++);
-	}
-	getchar();
-}
-
-
 char *str_dup(const char *s) {
 	char *d = (char *)malloc(strlen(s) + 1);
 	if (d)
 		strcpy(d, s);
 	return d;
-}
-
-int str_match_any(char *s, char *t[])
-{
-	while (*t)
-	if (strcmp(s, *t++) == 0)
-		return 1;
-	return 0;
 }
 
 /* searches for c in s */
